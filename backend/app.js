d7368346f8dfc7680f7ccc5f1adbba3f36adaa11
yogeshpatel12 <yp766104@gmail.com>
2024-05-27 14:01:23 +0530

    Updated folder structure and added dependencies.

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,41 @@
 import express from 'express'
-import products from './data/product.js'
-import connectionToDb from './db.js';
+import cors from "cors";
+import { config } from "dotenv";
 import colors from 'colors'
 
+import products from './data/product.js'
+// import connectionToDb from './db.js';
 
 
+config();
+// connectionToDb();
 const app =  express() ;
+const PORT=process.env.PORT?process.env.PORT:5000
 
-connectionToDb();
+app.use(
+  cors({
+    origin: [process.env.FRONTEND],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+app.use(express.json())
+
+
 
 app.get('/new', (req,res)=>{
     res.send('hello server is running')
 })
 
-app.get('/products',(req,res)=>{
+app.get('/api/products',(req,res)=>{
 res.json(products)
 })
 
-app.get('/products/:id', (req,res)=>{
+app.get('/api/products/:id', (req,res)=>{
   const item = products.find((p)=>
   p._id === req.params.id)
   res.json(item)
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on the port : 3000".rainbow)
+app.listen(PORT,()=>{
+    console.log(`Server is running on the port : ${PORT}`.rainbow)
 })
